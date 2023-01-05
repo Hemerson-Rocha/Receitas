e7700ae77400f45367419dd692783510b35807db
Hemerson-Rocha <hemerson.me@gmail.com>
2023-01-05 15:59:53 -0300

    configurações para deploy

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -13,6 +13,8 @@ import os, sys
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gn)=eaa)q#_uhqtrn-a0p1&471=y5%cl6(&#l@k=*#=p*_b$qe'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+# SECRET_KEY = 'django-insecure-gn)=eaa)q#_uhqtrn-a0p1&471=y5%cl6(&#l@k=*#=p*_b$qe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -44,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,6 +82,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': dj_database_url.config(         default='postgresql://postgres:postgres@localhost:5432/receitas',
+    conn_max_age=600    )}
 
 # DATABASES = {
 #     'default': {
@@ -128,11 +140,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'alurareceita/static')
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'alurareceita/static')
+# ]
+
+STATIC_URL = '/static/'
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
